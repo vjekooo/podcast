ea6ecf6d2d436852a514a0e819143355e45751c0
Vjeko Pavičin <vjeko.pavicin@gmail.com>
2019-03-16 12:00:15 +0100

    navigation tabs

---
+++ Podcast/MainTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  Podcast
+//
+//  Created by Vjeko on 16/03/2019.
+//  Copyright © 2019 Vjeko. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        viewControllers = [
+            generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search") ),
+            generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- Helper functions
+    
+    fileprivate func generateNavigationController(
+        with rootViewController: UIViewController,
+        title: String,
+        image: UIImage) -> UIViewController
+    {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
+    }
+}
