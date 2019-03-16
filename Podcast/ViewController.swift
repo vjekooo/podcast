ed25fa2798dee3f5b22555c531c24e58a78afcb3
Vjeko Pavičin <vjeko.pavicin@gmail.com>
2019-03-16 11:04:36 +0100

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Vjeko on 16/03/2019.
+//  Copyright © 2019 Vjeko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
