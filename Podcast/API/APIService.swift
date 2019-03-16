36067a5773c35cab5ef63c4d26e82aa018d92851
Vjeko Pavičin <vjeko.pavicin@gmail.com>
2019-03-16 15:13:15 +0100

    api call refactor

---
+++ Podcast/API/APIService.swift
@@ -0,0 +1,44 @@
+//
+//  APIService.swift
+//  Podcast
+//
+//  Created by Vjeko on 16/03/2019.
+//  Copyright © 2019 Vjeko. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    // singleton
+    static let shared = APIService()
+    
+    func fetchPodcasts(searchText: String, completionHandler: @escaping ([Podcast]) -> ()) {
+        
+        let url = "https://itunes.apple.com/search"
+        
+        let parameters = ["term": searchText, "media": "podcast"]
+        
+        Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            if let err = dataResponse.error {
+                print("Failed to yahooo", err)
+                return
+            }
+            guard let data = dataResponse.data else {return}
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResults.self, from: data)
+                
+                completionHandler(searchResult.results)
+                
+            } catch let decodeErr {
+                print("Failed to decode", decodeErr)
+            }
+        }
+    }
+    
+    struct SearchResults: Decodable {
+        let resultCount: Int
+        let results: [Podcast]
+    }
+}
--- Podcast/Controllers/PodcastsSearchController.swift
+++ Podcast/Controllers/PodcastsSearchController.swift
@@ -41,33 +41,12 @@ class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
         
-        let url = "https://itunes.apple.com/search"
-        
-        let parameters = ["term": searchText, "media": "podcast"]
-        
-        Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
-            if let err = dataResponse.error {
-                print("Failed to yahooo", err)
-                return
-            }
-            guard let data = dataResponse.data else {return}
-          
-            do {
-                let searchResult = try JSONDecoder().decode(SearchResults.self, from: data)
-               
-                self.podcasts = searchResult.results
-                self.tableView.reloadData()
-            } catch let decodeErr {
-                print("Failed to decode", decodeErr)
-            }
+        APIService.shared.fetchPodcasts(searchText: searchText) { (podcasts) in
+            self.podcasts = podcasts
+            self.tableView.reloadData()
         }
-    }
-    
-    struct SearchResults: Decodable {
-        let resultCount: Int
-        let results: [Podcast]
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
